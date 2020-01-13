4511451c92e1f09e1399914a81589d45c446c467
leksoid <aliaksei.taran@gmail.com>
2020-01-12 23:06:38 -0500

    Add test for contact creation

---
+++ contact.py
@@ -0,0 +1,9 @@
+class Contact:
+    def __init__(self, first_name, last_name, title, company, primary_address, mobile_number, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.title = title
+        self.company = company
+        self.primary_address = primary_address
+        self.mobile_number = mobile_number
+        self.email = email
