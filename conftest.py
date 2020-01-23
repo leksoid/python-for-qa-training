cec207f4b8a297df8b4c95aa2db761fa11f0c02e
leksoid <aliaksei.taran@gmail.com>
2020-01-22 22:16:57 -0500

    Renamed test files, so to be able to run as a suite

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import pytest
 from fixture.application import Application
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture()
 def app(request):
     fixture = Application()  # initialization of fixture
     request.addfinalizer(fixture.tear_down)  # how the fixture should be destroyed
